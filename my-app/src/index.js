b1a20d0a34762f2dfbc57c50f2001d3bc7ad6405
nazmisani <nazminouval@gmail.com>
2025-05-02 18:35:36 +0700

    chore: update dependencies and import paths for Backpack stylesheets
    - Added @skyscanner/backpack-web and bpk-stylesheets as dependencies in package.json.
    - Updated import paths in index.js to use the new Backpack stylesheets from @skyscanner/backpack-web.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
-import 'bpk-stylesheets/base';
-import 'bpk-stylesheets/base.css';
-import 'bpk-stylesheets/font';
-import 'bpk-stylesheets/font.css';
+import '@skyscanner/backpack-web/bpk-stylesheets/base';
+import '@skyscanner/backpack-web/bpk-stylesheets/base.css';
+import '@skyscanner/backpack-web/bpk-stylesheets/font';
+import '@skyscanner/backpack-web/bpk-stylesheets/font.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
